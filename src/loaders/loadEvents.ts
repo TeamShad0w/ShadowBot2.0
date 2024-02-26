04487a57bdf918af84b961d0e6b8e7bfe7987d2b
C0dex73 <c0d3x73@gmail.com>
2024-02-26 15:40:38 +0100

    added a todo

--- src/loaders/loadEvents.ts
+++ src/loaders/loadEvents.ts
@@ -2,5 +2,6 @@ import Discord from 'discord.js';
 
 //TODO : jsDoc
 export default async (bot:Discord.Client) : Promise<number|string> => {
+    // TODO : load events (see https://github.com/C0dex73/ShadowBot/blob/main/loader/loadEvents.js)
     return 1;
 }
