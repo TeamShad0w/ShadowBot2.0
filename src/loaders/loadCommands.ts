ac1e93f51c22a838ab1bc1d96d8f19cb865e59a2
Shaden <gabicollin91910@gmail.com>
2024-02-26 16:27:10 +0100

    Added loadCommands

--- src/loaders/loadCommands.ts
+++ src/loaders/loadCommands.ts
@@ -1,9 +1,29 @@
 import Discord from 'discord.js';
+import fs from "fs";
+
 
 //TODO : jsDoc
 export default async (bot:Discord.Client):Promise<number|string> => {
     
     // TODO : loadCommands (see https://github.com/C0dex73/ShadowBot/blob/main/loader/loadCommands.js)
+    let err = undefined
 
-    return 1;
+    fs.readdirSync("./commands").filter(f => f.endsWith(".js")).forEach(async file => {
+
+        let command = require(`./commands/${file}`);
+
+        if(!command.name || typeof command.name !== "string") {
+            err = (`Incorect name for command ${file.slice(0, file.length -3)}.`)
+            return false;
+        } else {
+            bot.commands.set(command.name, command)
+            console.log(`COMMANDLOAD : ${file} loaded.`)
+        }       
+    })  
+    if (err) {
+        return err
+    } else {
+        return 1
+    }
+     ;
 }
