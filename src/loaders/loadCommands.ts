afad4bc88c4a8d4e8a6e632fc6646503c7e0b527
Shaden <gabicollin91910@gmail.com>
2024-02-26 16:37:02 +0100

    merging branches

--- src/loaders/loadCommands.ts
+++ src/loaders/loadCommands.ts
@@ -1,10 +1,10 @@
 import Discord from 'discord.js';
-<<<<<<< HEAD
+
 import fs from "fs";
 
-=======
+
 import ClientWithCommands from '../utils/clientWithCommands';
->>>>>>> 9dcc45c90560b6e8c3a398d7778a8b99151801d3
+
 
 //TODO : jsDoc
 export default async (bot:ClientWithCommands):Promise<number|string> => {
