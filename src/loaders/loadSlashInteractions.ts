fd4f6d6cc42cde3d82641711a3ec96ea776f2788
C0dex73 <c0d3x73@gmail.com>
2024-02-26 16:51:06 +0100

    added loader for slash commands

--- src/loaders/loadSlashInteractions.ts
+++ src/loaders/loadSlashInteractions.ts
@@ -3,5 +3,26 @@ import ClientWithCommands from '../utils/clientWithCommands';
 
 //TODO : jsDoc
 export default async (bot:ClientWithCommands) : Promise<number|string> => {
+    
+    if(bot.user === null) { return "Error while loading SlashCommands : bot.user is null"; }
+
+    let commands:Array<Discord.SlashCommandBuilder> = [];
+
+    bot.commands.forEach((command:any) => {
+        let slashCommand:Discord.SlashCommandBuilder = new Discord.SlashCommandBuilder()
+            .setName(command.name)
+            .setDescription(command.description)
+            .setDMPermission(command.dm)
+            .setDefaultMemberPermissions(command.permission);
+
+        commands.push(slashCommand);
+
+        console.log("Command : " + command.name + " is loaded");
+    })
+
+    const rest = new Discord.REST({version: '10'}).setToken(bot.token == null ? "" : bot.token);
+
+    await rest.put(Discord.Routes.applicationCommands(bot.user.id), {body: commands});
+
     return 1;
 }
