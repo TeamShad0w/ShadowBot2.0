661efc6b818f3c4c828fa84c750d08d6fb3e9d9b
Shaden <gabicollin91910@gmail.com>
2024-02-28 19:32:18 +0100

    added help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,7 @@
 import Discord, { messageLink, Options } from 'discord.js';
 import ClientWithCommands from '../utils/clientWithCommands';
+import path from "path";
+import fs from "fs"
 
 // TODO : jsDoc
 export default {
@@ -10,6 +12,28 @@ export default {
 
     //TODO : find type of interaction and replace any with it
     async run(bot:ClientWithCommands, interaction:any): Promise<void> {
-        await interaction.reply("Here the list of the commands and a short description : \n /ping : and the bot will reply pong \n /setup : same as Ping actually ");
+
+        let Way:string = path.dirname(path.dirname(__filename));
+
+        let fmsg = "";
+
+fmsg += "Here the list of the commands and a short description :";
+fs.readdir(`${Way}/commands`,(err,files) => files.filter(f => f.endsWith("js") || f.endsWith("ts")).forEach(async file => {
+    let command = await require(`${Way}/commands/${file}`).default;
+    console.log(command)
+    fmsg += `\r\n> **${command.name}** : *${command.description}*`;
+    console.log(fmsg)
+
     }
+    
+)
+
+// TODO: I NEED HELP IN HELP PLS
+
+/*
+interaction.reply(fmsg)
+*/
+)
+
 }
+};
