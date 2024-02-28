e71426662b9f0dbb78ba5a8962d4fcbebc41b5e6
C0dex73 <c0d3x73@gmail.com>
2024-02-28 14:59:54 +0100

    created guildHandler

--- src/utils/clientWithCommands.ts
+++ src/utils/clientWithCommands.ts
@@ -1,8 +1,10 @@
 import Discord from 'discord.js';
 import ICommand from './command';
+import { IGlobalGuildContainer } from './guildHandler';
 import Iconfig from './configHandler';
 
 export default interface ClientWithCommands extends Discord.Client {
     commands: Discord.Collection<string, ICommand>;
+    guildHandlers : Discord.Collection<Discord.Guild, IGlobalGuildContainer>;
     config : Iconfig;
 }
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,28 @@
-let file = "test.js"
+import { LogLevel } from '../src/utils/consoleHandler';
+import Discord from 'discord.js'
 
-console.log(file.split(".js").join(""))
+interface IdatabaseDataHolder {
+    url : string;
+    APIKey : string;
+}
+
+class guildHandler {
+    id : Discord.Snowflake;
+    database : IdatabaseDataHolder;
+    logChannelID : Discord.Snowflake;
+    logLevel : LogLevel;
+
+    constructor(_id:Discord.Snowflake, _databse:IdatabaseDataHolder = { url : "-1", APIKey : "-1" }, _logChannelID:Discord.Snowflake = "-1", _logLevel:LogLevel = LogLevel.Info) {
+        this.id = _id;
+        this.database = _databse;
+        this.logChannelID = _logChannelID;
+        this.logLevel = _logLevel;
+    }
+}
+
+let guilds:Array<guildHandler> = [];
+
+guilds.push(new guildHandler("54"))
+guilds.push(new guildHandler("42"))
+
+console.log(JSON.stringify(guilds, undefined, 4));
