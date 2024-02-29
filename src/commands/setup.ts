12a80c7a16ef3af46ded4b8789e6c111a2131b05
C0dex73 <c0d3x73@gmail.com>
2024-02-29 11:11:42 +0100

    found interaction type in run function of commands

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -7,8 +7,7 @@ export default {
     permission : Discord.PermissionFlagsBits.ManageGuild,
     dm : true,
 
-    //TODO : find type of interaction and replace any with it
-    async run(bot:ClientWithCommands, interaction:any): Promise<void> {
+    async run(bot:ClientWithCommands, interaction:Discord.ChatInputCommandInteraction): Promise<void> {
         await interaction.reply("pong");
     }
 }
