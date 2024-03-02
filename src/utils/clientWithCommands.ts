5f59ec785c1da80639dda25650a5c9012453b1cf
C0dex73 <c0d3x73@gmail.com>
2024-03-02 13:11:13 +0100

    transformed client with command into a class and rework around the naming of configHandler

--- src/utils/clientWithCommands.ts
+++ src/utils/clientWithCommands.ts
@@ -1,11 +1,18 @@
 import Discord from 'discord.js';
 import ICommand from './command';
 import { IGlobalGuildContainer } from './guildHandler';
-import Iconfig from './configHandler';
+import ConfigHandler from './configHandler';
+import { Iconfig } from './configHandler';
 
 // TODO : jsDoc
-export default interface ClientWithCommands extends Discord.Client {
-    commands: Discord.Collection<string, ICommand>;
-    guildHandlers : Discord.Collection<Discord.Guild, IGlobalGuildContainer>;
-    config : Iconfig;
+export default class ClientWithCommands extends Discord.Client {
+    public commands: Discord.Collection<string, ICommand>;
+    public guildHandlers : Discord.Collection<Discord.Guild, IGlobalGuildContainer>;
+    public configHandler : ConfigHandler;
+    constructor(Config:Iconfig, options:any){
+        super(options);
+        this.commands = new Discord.Collection<string, ICommand>();
+        this.guildHandlers = new Discord.Collection<Discord.Guild, IGlobalGuildContainer>();
+        this.configHandler = new ConfigHandler(Config);
+    }
 }
--- src/utils/configHandler.ts
+++ src/utils/configHandler.ts
@@ -12,15 +12,15 @@ export interface Iconfig {
 
 // TODO : jsDoc
 export default class ConfigHandler {
-    config : Iconfig;
+    value : Iconfig;
 
     constructor(_config : Iconfig) {
-        this.config = _config;
+        this.value = _config;
     };
 
     // TODO : jsDoc
     modify = async (modifingFunction:(config:Iconfig)=>Promise<Iconfig>|Iconfig) => {
-        this.config = await modifingFunction(this.config);
+        this.value = await modifingFunction(this.value);
         this.write();
     }
 
@@ -28,7 +28,7 @@ export default class ConfigHandler {
     write = async () : Promise<string | number> => {
         try{
             let Way:string = path.dirname(path.dirname(__filename));
-            fs.writeFile(`${Way}/config.json`, JSON.stringify(this.config, undefined, 4), () => print("config.json modified", LogLevel.Log));
+            fs.writeFile(`${Way}/config.json`, JSON.stringify(this.value, undefined, 4), () => print("config.json modified", LogLevel.Log));
         }catch(err) {
             return "Wasn't able to write config.json : " + err;
         }
