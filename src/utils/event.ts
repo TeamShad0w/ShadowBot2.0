b4fc96f5528d224557dc7f4fee683b5981057df3
Codex73 <95096929+C0dex73@users.noreply.github.com>
2024-03-23 22:40:10 +0100

    Added jsDoc

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,7 +1,9 @@
 import ClientWithCommands from './clientWithCommands';
 import { Awaitable } from 'discord.js';
 
-// TODO : jsDoc
+/**
+ * The architecture of an event object (modules of the event/ directory).
+ */
 export default interface Ievent{
     listener : (bot:ClientWithCommands, ...args : any[]) => Awaitable<void>;
 }
